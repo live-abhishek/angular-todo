95c7ab45a3a8b88df271809143b559cf98f65883
Masterchief <live.abhishek@outlook.com>
2019-02-26 19:18:56 +0530

    Chapter-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TodoDataService } from "./todo-data.service";
+import { Todo } from "./todo";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [TodoDataService],
 })
 export class AppComponent {
-  title = 'angular-todo';
+  newTodo: Todo = new Todo();
+  title = "angular-todo";
+  private todoDataService: TodoDataService;
+  constructor(todoDataService: TodoDataService) {
+    this.todoDataService = todoDataService;
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
